6b6d333a9aa4c77291f17dc894d3ce4a97e13fb1
asif2508 <rakibul4210@gmail.com>
2025-04-19 16:46:47 +0600

    class 3

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 const app = express();
-
+require('dotenv').config();
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
+// database
 // get,post,delete, update
 
-app.get("/", (req, res) => {
-  res.send("This is get request");
-})
+const databaseURL = process.env.DB_URL;
 
-app.post('/', (req, res)=>{
-  res.send("This is post request")
-})
+console.log("db", databaseURL)
 
-app.delete('/', (req, res)=>{
-  res.send("This is delete request")
-})
+const main = async () => {
+  try {
+    const client = new MongoClient(databaseURL);
+    await client.connect();
+    console.log("database connected");
+    const database = client.db("web-dev");
+    const Users = database.collection("users");
 
-app.patch('/', (req, res)=>{
-  res.send("This is update request")
-})
+    app.post("/create-user", async (req, res) => {
+      const data = req.body;
+      const result = await Users.insertOne(data);
+      if(result){
+        res.send({
+          status: "success",
+          message: "user created successfully",
+          data: result
+        })
+      }else{
+        res.send("error")
+      }
+    });
+
+    app.get('/get-users', async(req, res)=>{
+      const result = await Users.find({}).toArray();
+      res.send(result);
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
