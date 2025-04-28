17ba12cc1a6eb279e3e01f14606f6a6c725e449c
asif2508 <rakibul4210@gmail.com>
2025-04-28 15:15:07 +0800

    class 4

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const bcrypt = require('bcrypt')
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 // middleware
 
 app.use(cors());
@@ -13,8 +14,8 @@ app.use(express.json());
 
 const databaseURL = process.env.DB_URL;
 
-console.log("db", databaseURL)
-
+console.log("db", databaseURL);
+const saltRounds = 10
 const main = async () => {
   try {
     const client = new MongoClient(databaseURL);
@@ -23,25 +24,38 @@ const main = async () => {
     const database = client.db("web-dev");
     const Users = database.collection("users");
 
-    app.post("/create-user", async (req, res) => {
+    app.post("/signup", async (req, res) => {
       const data = req.body;
-      const result = await Users.insertOne(data);
-      if(result){
+      console.log(data.email, "data");
+      const isExist = await Users.findOne({ email: data.email });
+      if (isExist) {
         res.send({
-          status: "success",
-          message: "user created successfully",
-          data: result
-        })
-      }else{
-        res.send("error")
+          status: "failed",
+          message: "User already exists with this email!",
+        });
+      } else {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+          bcrypt.hash(data.password, salt, async function(err, hash) {
+            const result = await Users.insertOne({...data, password: hash});
+            if (result) {
+              res.send({
+                status: "success",
+                message: "user created successfully",
+                data: result,
+              });
+            } else {
+              res.send("error");
+            }
+          });
+      });
+        
       }
     });
 
-    app.get('/get-users', async(req, res)=>{
+    app.get("/get-users", async (req, res) => {
       const result = await Users.find({}).toArray();
       res.send(result);
-    })
-
+    });
   } catch (error) {
     console.log(error);
   }
